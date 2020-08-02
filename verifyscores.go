a21264c396330c75553389d74a74074562203e55
Lucas Marzocco <lucasmarzocco@gmail.com>
2020-08-02 16:44:07 -0700

    adding all API endpoints

--- verifyscores.go
+++ verifyscores.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -21,66 +20,68 @@ var (
 	TOURNAMENTS = "/USBC.Search.Services/api/v1/tournamentaverages"
 )
 
-func IDHandler(w http.ResponseWriter, r *http.Request) {
+func query(url string, r *http.Request) []byte {
+	req, err := http.NewRequest("GET", url, nil)
 
-	req, err := http.NewRequest("GET", SITE + ID, nil)
 	if err != nil {
-		log.Print(err)
 		log.Fatal(err)
 	}
 
 	s := r.URL.Query()
-
 	prefix := s["prefix"][0]
 	suffix := s["suffix"][0]
 
 	q := req.URL.Query()
 	q.Add("prefix", prefix)
 	q.Add("suffix", suffix)
+	q.Add("size", string(1000))
 	req.URL.RawQuery = q.Encode()
 
-	fmt.Println(req.URL.String())
-
 	data, err := http.Get(req.URL.String())
-
 	body, err := ioutil.ReadAll(data.Body)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	return body
+}
 
+func IDHandler(w http.ResponseWriter, r *http.Request) {
+	body := query(SITE + ID, r)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
 }
 
 func AverageHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + AVERAGES, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 func RerateHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + RERATE, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func LeagueHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + LEAGUES, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 func MembershipHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + MEMBERSHIPS, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func AchievementHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + ACHIEVEMENTS, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
 
 func TournamentHandler(w http.ResponseWriter, r *http.Request) {
-
-
+	body := query(SITE + TOURNAMENTS, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
-
 
 func main() {
 
@@ -91,5 +92,11 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/id", IDHandler)
+	r.HandleFunc("/averages", AverageHandler)
+	r.HandleFunc("/rerates", RerateHandler)
+	r.HandleFunc("/leagues", LeagueHandler)
+	r.HandleFunc("/memberships", MembershipHandler)
+	r.HandleFunc("/achievements", AchievementHandler)
+	r.HandleFunc("/tournaments", TournamentHandler)
 	log.Fatal(http.ListenAndServe(":" + port, r))
 }
